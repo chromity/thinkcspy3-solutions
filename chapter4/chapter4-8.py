b1168ca5709b487bb9ab4c337322daa5b41d161e
Michael Daryl Mayo <michaeldaryl.code@gmail.com>
2017-05-09 08:16:24 -0700

    Adds solution to exercise 8 in chapter 4

---
+++ chapter4/chapter4-8.py
@@ -0,0 +1,9 @@
+# Write a function area_of_circle(r) which returns the area of a circle of
+# radius r.
+
+def area_of_circle(r):
+    """Accepts 'r' as a variable for radius then computes and returns the value
+        of the area of the circle with radius 'r'."""
+    area = 2 * 3.1416 * r  # using formula a_of_circle = 2 * pi * radius
+
+    return area
